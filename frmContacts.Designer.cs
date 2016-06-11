9bb05e5239395c8902fa64fd2b9d1e46fbe550f1
taraeicher <taraeicher@yahoo.com>
2016-06-10 19:05:12 -0500

    Add files via upload

---
+++ frmContacts.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace CSMailOrder
+{
+    partial class frmContacts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDepartment = new System.Windows.Forms.Label();
+            this.lblContact = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.btnCustomerRelations = new System.Windows.Forms.Button();
+            this.btnMarketing = new System.Windows.Forms.Button();
+            this.btnOrderProcessing = new System.Windows.Forms.Button();
+            this.btnShipping = new System.Windows.Forms.Button();
+            this.lblCRContact = new System.Windows.Forms.Label();
+            this.lblCRPhone = new System.Windows.Forms.Label();
+            this.lblMarketingContact = new System.Windows.Forms.Label();
+            this.lblMarketingPhone = new System.Windows.Forms.Label();
+            this.lblOPContact = new System.Windows.Forms.Label();
+            this.lblOPPhone = new System.Windows.Forms.Label();
+            this.lblShippingContact = new System.Windows.Forms.Label();
+            this.lblShippingPhone = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(76, 28);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(231, 20);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Custom Supplies Mail Order";
+            // 
+            // lblDepartment
+            // 
+            this.lblDepartment.AutoSize = true;
+            this.lblDepartment.Location = new System.Drawing.Point(47, 82);
+            this.lblDepartment.Name = "lblDepartment";
+            this.lblDepartment.Size = new System.Drawing.Size(62, 13);
+            this.lblDepartment.TabIndex = 1;
+            this.lblDepartment.Text = "Department";
+            // 
+            // lblContact
+            // 
+            this.lblContact.AutoSize = true;
+            this.lblContact.Location = new System.Drawing.Point(151, 82);
+            this.lblContact.Name = "lblContact";
+            this.lblContact.Size = new System.Drawing.Size(102, 13);
+            this.lblContact.TabIndex = 2;
+            this.lblContact.Text = "Department Contact";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(274, 82);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(98, 13);
+            this.lblPhone.TabIndex = 3;
+            this.lblPhone.Text = "Telephone Number";
+            // 
+            // btnCustomerRelations
+            // 
+            this.btnCustomerRelations.Location = new System.Drawing.Point(12, 114);
+            this.btnCustomerRelations.Name = "btnCustomerRelations";
+            this.btnCustomerRelations.Size = new System.Drawing.Size(120, 23);
+            this.btnCustomerRelations.TabIndex = 4;
+            this.btnCustomerRelations.Text = "&Customer Relations";
+            this.btnCustomerRelations.UseVisualStyleBackColor = true;
+            this.btnCustomerRelations.Click += new System.EventHandler(this.btnCustomerRelations_Click);
+            // 
+            // btnMarketing
+            // 
+            this.btnMarketing.Location = new System.Drawing.Point(12, 153);
+            this.btnMarketing.Name = "btnMarketing";
+            this.btnMarketing.Size = new System.Drawing.Size(120, 23);
+            this.btnMarketing.TabIndex = 5;
+            this.btnMarketing.Text = "&Marketing";
+            this.btnMarketing.UseVisualStyleBackColor = true;
+            this.btnMarketing.Click += new System.EventHandler(this.btnMarketing_Click);
+            // 
+            // btnOrderProcessing
+            // 
+            this.btnOrderProcessing.Location = new System.Drawing.Point(12, 194);
+            this.btnOrderProcessing.Name = "btnOrderProcessing";
+            this.btnOrderProcessing.Size = new System.Drawing.Size(120, 23);
+            this.btnOrderProcessing.TabIndex = 6;
+            this.btnOrderProcessing.Text = "&Order Processing";
+            this.btnOrderProcessing.UseVisualStyleBackColor = true;
+            this.btnOrderProcessing.Click += new System.EventHandler(this.btnOrderProcessing_Click);
+            // 
+            // btnShipping
+            // 
+            this.btnShipping.Location = new System.Drawing.Point(12, 237);
+            this.btnShipping.Name = "btnShipping";
+            this.btnShipping.Size = new System.Drawing.Size(120, 23);
+            this.btnShipping.TabIndex = 7;
+            this.btnShipping.Text = "&Shipping";
+            this.btnShipping.UseVisualStyleBackColor = true;
+            this.btnShipping.Click += new System.EventHandler(this.btnShipping_Click);
+            // 
+            // lblCRContact
+            // 
+            this.lblCRContact.Location = new System.Drawing.Point(151, 114);
+            this.lblCRContact.Name = "lblCRContact";
+            this.lblCRContact.Size = new System.Drawing.Size(100, 23);
+            this.lblCRContact.TabIndex = 8;
+            // 
+            // lblCRPhone
+            // 
+            this.lblCRPhone.Location = new System.Drawing.Point(272, 114);
+            this.lblCRPhone.Name = "lblCRPhone";
+            this.lblCRPhone.Size = new System.Drawing.Size(100, 23);
+            this.lblCRPhone.TabIndex = 9;
+            // 
+            // lblMarketingContact
+            // 
+            this.lblMarketingContact.Location = new System.Drawing.Point(151, 153);
+            this.lblMarketingContact.Name = "lblMarketingContact";
+            this.lblMarketingContact.Size = new System.Drawing.Size(100, 23);
+            this.lblMarketingContact.TabIndex = 10;
+            // 
+            // lblMarketingPhone
+            // 
+            this.lblMarketingPhone.Location = new System.Drawing.Point(272, 153);
+            this.lblMarketingPhone.Name = "lblMarketingPhone";
+            this.lblMarketingPhone.Size = new System.Drawing.Size(100, 23);
+            this.lblMarketingPhone.TabIndex = 11;
+            // 
+            // lblOPContact
+            // 
+            this.lblOPContact.Location = new System.Drawing.Point(151, 194);
+            this.lblOPContact.Name = "lblOPContact";
+            this.lblOPContact.Size = new System.Drawing.Size(100, 23);
+            this.lblOPContact.TabIndex = 12;
+            // 
+            // lblOPPhone
+            // 
+            this.lblOPPhone.Location = new System.Drawing.Point(272, 194);
+            this.lblOPPhone.Name = "lblOPPhone";
+            this.lblOPPhone.Size = new System.Drawing.Size(100, 23);
+            this.lblOPPhone.TabIndex = 13;
+            // 
+            // lblShippingContact
+            // 
+            this.lblShippingContact.Location = new System.Drawing.Point(151, 237);
+            this.lblShippingContact.Name = "lblShippingContact";
+            this.lblShippingContact.Size = new System.Drawing.Size(100, 23);
+            this.lblShippingContact.TabIndex = 14;
+            // 
+            // lblShippingPhone
+            // 
+            this.lblShippingPhone.Location = new System.Drawing.Point(272, 237);
+            this.lblShippingPhone.Name = "lblShippingPhone";
+            this.lblShippingPhone.Size = new System.Drawing.Size(100, 23);
+            this.lblShippingPhone.TabIndex = 15;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(38, 307);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(62, 13);
+            this.lblName.TabIndex = 16;
+            this.lblName.Text = "Tara Eicher";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(265, 288);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 17;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmContacts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 341);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblShippingPhone);
+            this.Controls.Add(this.lblShippingContact);
+            this.Controls.Add(this.lblOPPhone);
+            this.Controls.Add(this.lblOPContact);
+            this.Controls.Add(this.lblMarketingPhone);
+            this.Controls.Add(this.lblMarketingContact);
+            this.Controls.Add(this.lblCRPhone);
+            this.Controls.Add(this.lblCRContact);
+            this.Controls.Add(this.btnShipping);
+            this.Controls.Add(this.btnOrderProcessing);
+            this.Controls.Add(this.btnMarketing);
+            this.Controls.Add(this.btnCustomerRelations);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblContact);
+            this.Controls.Add(this.lblDepartment);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "frmContacts";
+            this.Text = "CS Mail Order Contact Information";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDepartment;
+        private System.Windows.Forms.Label lblContact;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Button btnCustomerRelations;
+        private System.Windows.Forms.Button btnMarketing;
+        private System.Windows.Forms.Button btnOrderProcessing;
+        private System.Windows.Forms.Button btnShipping;
+        private System.Windows.Forms.Label lblCRContact;
+        private System.Windows.Forms.Label lblCRPhone;
+        private System.Windows.Forms.Label lblMarketingContact;
+        private System.Windows.Forms.Label lblMarketingPhone;
+        private System.Windows.Forms.Label lblOPContact;
+        private System.Windows.Forms.Label lblOPPhone;
+        private System.Windows.Forms.Label lblShippingContact;
+        private System.Windows.Forms.Label lblShippingPhone;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ frmContacts.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Project:  Ch. 1 Case Study CS Mail Order
+//Programmer:   Tara Eicher
+//Date: 7 September 2010
+//Description:  This project displays the name and phone number of the
+//      contact person for each department.
+namespace CSMailOrder
+{
+    public partial class frmContacts : Form
+    {
+        public frmContacts()
+        {
+            InitializeComponent();
+        }
+//Display the name and phone number for Customer Relations.
+        private void btnCustomerRelations_Click(object sender, EventArgs e)
+        {
+            lblCRContact.Text = "Tricia Mills";
+            lblCRPhone.Text = "500-1111";
+        }
+//Display the name and phone number for Marketing.
+        private void btnMarketing_Click(object sender, EventArgs e)
+        {
+            lblMarketingContact.Text = "Michelle Rigner";
+            lblMarketingPhone.Text = "500-2222";
+        }
+//Display the name and phone number for Order Processing.
+        private void btnOrderProcessing_Click(object sender, EventArgs e)
+        {
+            lblOPContact.Text = "Kenna DeVoss";
+            lblOPPhone.Text = "500-2222";
+        }
+//Display the name and phone number for Shipping.
+        private void btnShipping_Click(object sender, EventArgs e)
+        {
+            lblShippingContact.Text = "Eric Andrews";
+            lblShippingPhone.Text = "500-4444";
+        }
+//Close the program.
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
